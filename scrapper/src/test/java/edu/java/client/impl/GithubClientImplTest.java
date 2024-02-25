fef3573d81027b48116335608f596c992d73baaf
Никита <nikproserg@gmail.com>
2024-02-25 17:50:40 +0800

    add wiremock tests

---
+++ scrapper/src/test/java/edu/java/client/impl/GithubClientImplTest.java
@@ -0,0 +1,50 @@
+package edu.java.client.impl;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import edu.java.configuration.ClientConfiguration;
+import edu.java.response.RepositoryResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WireMockTest(httpPort = 8082)
+public class GithubClientImplTest {
+
+    private GithubClientImpl githubClient;
+
+    @BeforeEach
+    public void setUp() {
+        githubClient = new ClientConfiguration().githubClient("http://localhost:8082");
+    }
+
+    @Test
+    public void testFetchPost() {
+        String ownerUserName = "Prohkit";
+        String repositoryName = "java-backend-course-2-link-tracker";
+        stubFor(get(urlEqualTo("/repos/" + ownerUserName + "/" + repositoryName))
+            .willReturn(ok()
+                .withHeader("Content-Type", "application/json")
+                .withBody("""
+                        {
+                            "id": 1642028,
+                            "full_name": "Prohkit/java-backend-course-2-link-tracker"
+                        }
+                    """)
+            ));
+        RepositoryResponse response = githubClient.fetchRepository(ownerUserName, repositoryName);
+
+        assertThat(response)
+            .isNotNull()
+            .extracting(
+                RepositoryResponse::repositoryId,
+                RepositoryResponse::fullName
+            ).containsExactly(
+                1642028L,
+                "Prohkit/java-backend-course-2-link-tracker"
+            );
+    }
+}
---
+++ scrapper/src/test/java/edu/java/client/impl/StackoverflowClientImplTest.java
@@ -0,0 +1,63 @@
+package edu.java.client.impl;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import edu.java.client.StackOverflowClient;
+import edu.java.configuration.ClientConfiguration;
+import edu.java.response.QuestionResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WireMockTest(httpPort = 8082)
+public class StackoverflowClientImplTest {
+
+    private StackOverflowClient stackOverflowClient;
+
+    @BeforeEach
+    public void setUp() {
+        stackOverflowClient = new ClientConfiguration().stackOverflowClient("http://localhost:8082");
+    }
+
+    @Test
+    public void testFetchPost() {
+        long questionId = 1642028L;
+        stubFor(get(urlEqualTo("/questions/" + questionId + "/?site=stackoverflow&filter=withbody"))
+            .willReturn(ok()
+                .withHeader("Content-Type", "application/json")
+                .withBody("""
+                    {
+                      "items": [
+                        {
+                            "question_id": 1642028,
+                            "title": "TITLE",
+                            "is_answered": true,
+                            "score": 20
+                        }
+                      ],
+                      "has_more": false,
+                      "quota_max": 10000,
+                      "quota_remaining": 9926
+                    }
+                    """)
+            ));
+        QuestionResponse response = stackOverflowClient.fetchQuestion(questionId);
+
+        assertThat(response)
+            .isNotNull()
+            .extracting(
+                questionResponse -> questionResponse.items().get(0).questionId(),
+                questionResponse -> questionResponse.items().get(0).title(),
+                questionResponse -> questionResponse.items().get(0).isAnswered(),
+                questionResponse -> questionResponse.items().get(0).score()
+            ).containsExactly(
+                questionId,
+                "TITLE",
+                true,
+                20
+            );
+    }
+}
