56381dc43ba7fcb9134f8f3120fb1d844e04bf57
Никита <nikproserg@gmail.com>
2024-03-01 18:22:19 +0800

    add BotController

---
+++ bot/src/main/java/edu/java/bot/controller/BotController.java
@@ -0,0 +1,21 @@
+package edu.java.bot.controller;
+
+import edu.java.bot.dto.LinkUpdate;
+import edu.java.bot.exception.BadRequestException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class BotController {
+    @PostMapping(consumes = "application/json", value = "/updates")
+    @ResponseStatus(HttpStatus.OK)
+    public void sendResponse(@RequestBody LinkUpdate linkUpdate) {
+        log.info("Отправка обновления на обработку");
+        throw new BadRequestException("Заглушка");
+    }
+}
