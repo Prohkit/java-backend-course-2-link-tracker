102d99de37a8aecb87ac8932ab14316cb8b6d15d
Никита <nikproserg@gmail.com>
2024-03-09 21:14:01 +0800

    edit runMigrations, update pom.xml

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,5 +1,18 @@
 package edu.java.scrapper;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
@@ -21,7 +34,22 @@ public abstract class IntegrationTest {
     }
 
     private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+        try {
+            Connection connection =
+                DriverManager.getConnection(c.getJdbcUrl(), c.getUsername(), c.getPassword());
+            Database database =
+                DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Liquibase liquibase =
+                new Liquibase(
+                    "master.xml",
+                    new DirectoryResourceAccessor(new File(".").toPath().resolve("../migrations")),
+                    database
+                );
+
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (SQLException | LiquibaseException | FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @DynamicPropertySource
