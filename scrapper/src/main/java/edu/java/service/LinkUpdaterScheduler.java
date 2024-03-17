64a4a831b3db4dfba85e853f6878ea2dce7e69ee
Никита <nikproserg@gmail.com>
2024-03-18 00:50:05 +0800

    add link updates, send updates to bot

--- scrapper/src/main/java/edu/java/service/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/service/LinkUpdaterScheduler.java
@@ -1,7 +1,14 @@
 package edu.java.service;
 
+import edu.java.client.BotClient;
 import edu.java.client.ClientsChain;
+import edu.java.domain.Link;
+import edu.java.dto.bot.LinkUpdate;
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +18,52 @@ public class LinkUpdaterScheduler {
 
     private final ClientsChain chain;
 
-    public LinkUpdaterScheduler(ClientsChain chain) {
+    private final BotClient botClient;
+
+    private final LinkService linkService;
+
+    private final TelegramChatService telegramChatService;
+
+    public LinkUpdaterScheduler(
+        ClientsChain chain,
+        BotClient botClient,
+        LinkService linkService,
+        TelegramChatService telegramChatService
+    ) {
         this.chain = chain;
+        this.botClient = botClient;
+        this.linkService = linkService;
+        this.telegramChatService = telegramChatService;
     }
+
+    @Value("${link-check-interval}")
+    private Duration linkCheckInterval;
 
     @Scheduled(fixedDelayString = "#{@scheduler.interval}")
     public void update() {
-        //chain.getUpdateInfo("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c-c");
-        //chain.getUpdateInfo("https://github.com/Prohkit/java-backend-course-2-link-tracker");
         log.info("start update");
+        List<Link> linksToUpdate = linkService.findLinksToCheck(linkCheckInterval);
+        for (Link link : linksToUpdate) {
+            String description = chain.getUpdateInfo(link);
+            linkService.updateLastUpdateCheckTime(link.getId());
+
+            Link linkFromDB = linkService.findLinkById(link.getId());
+
+            OffsetDateTime previousLastModifiedTime = linkFromDB.getLastModifiedTime();
+            if (previousLastModifiedTime.isBefore(link.getLastModifiedTime())) {
+                linkService.updateLastModifiedTime(
+                    link.getId(),
+                    link.getLastModifiedTime()
+                );
+            }
+            List<Long> telegramChatIds = telegramChatService.getTelegramChatIdsByLinkId(link.getId());
+            LinkUpdate linkUpdate = LinkUpdate.builder()
+                .id(link.getId())
+                .url(link.getUrl())
+                .description(description)
+                .tgChatIds(telegramChatIds)
+                .build();
+            botClient.sendUpdate(linkUpdate);
+        }
     }
 }
