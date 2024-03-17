07a708b8c12f70e295958199f1080d63fe7c90ba
Никита <nikproserg@gmail.com>
2024-03-18 00:47:19 +0800

    add services to scrapper

---
+++ scrapper/src/main/java/edu/java/service/jdbc/JdbcTelegramChatService.java
@@ -0,0 +1,49 @@
+package edu.java.service.jdbc;
+
+import edu.java.domain.Chat;
+import edu.java.domain.Link;
+import edu.java.repository.LinkRepository;
+import edu.java.repository.TelegramChatRepository;
+import edu.java.service.TelegramChatService;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JdbcTelegramChatService implements TelegramChatService {
+
+    private final TelegramChatRepository telegramChatRepository;
+
+    private final LinkRepository linkRepository;
+
+    public JdbcTelegramChatService(TelegramChatRepository telegramChatRepository, LinkRepository linkRepository) {
+        this.telegramChatRepository = telegramChatRepository;
+        this.linkRepository = linkRepository;
+    }
+
+    @Override
+    public void register(long telegramChatId) {
+        telegramChatRepository.addChat(Chat.builder().id(telegramChatId).build());
+    }
+
+    @Override
+    public void unregister(long telegramChatId) {
+        List<Long> linkIds = telegramChatRepository.removeChatLinkRelationships(telegramChatId);
+        for (Long linkId : linkIds) {
+            if (!linkRepository.areThereAnyChatLinkRelationshipsByLinkId(linkId)) {
+                Link linkToRemove = linkRepository.findLinkById(linkId);
+                linkRepository.removeLink(linkToRemove);
+            }
+        }
+        telegramChatRepository.removeChat(Chat.builder().id(telegramChatId).build());
+    }
+
+    public boolean isChatExists(long telegramChatId) {
+        return telegramChatRepository.isChatExists(Chat.builder().id(
+            telegramChatId).build());
+    }
+
+    @Override
+    public List<Long> getTelegramChatIdsByLinkId(long linkId) {
+        return telegramChatRepository.getTelegramChatIdsByLinkId(linkId);
+    }
+}
