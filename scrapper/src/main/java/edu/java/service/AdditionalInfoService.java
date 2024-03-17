08c4b6a4a7b51289391d3190a08f298356df8a28
Никита <nikproserg@gmail.com>
2024-03-18 04:16:15 +0800

    add AdditionalInfoService

---
+++ scrapper/src/main/java/edu/java/service/AdditionalInfoService.java
@@ -0,0 +1,9 @@
+package edu.java.service;
+
+import edu.java.domain.Link;
+
+public interface AdditionalInfoService {
+    void addAdditionalInfo(Link link);
+
+    void removeAdditionalInfo(Link link);
+}
